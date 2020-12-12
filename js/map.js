3e0e2c5753f081d54962a0668a4eb6086e5a9f5a
kamome-829 <tokunaga8695@gmail.com>
2020-12-12 14:47:58 +0900

    sered commit

--- js/map.js
+++ js/map.js
@@ -38,9 +38,7 @@ $(() => {
             .getCurrentPosition(mapsInit, showError, option);
     }
 
-    window.onload = function () {
-        getPosition();
-    }
+
     /*
         function mapsInit(position) {
           const lat = position.coords.latitude;
@@ -71,6 +69,18 @@ $(() => {
         oll_open();
     }
 
+    function mapsInit_2(lat, lng, map, name, place) {
+        // マップ表示の処理（省略）
+        map = new Microsoft.Maps.Map('#map', {
+            center: {
+                latitude: lat, longitude: lng
+            },
+            zoom: 8,
+        });
+        pushPin(lat, lng, map);
+        generateInfobox(lat, lng, map, name, place);
+    }
+
     function pushPin(lat, lng, map) {
         const location = new Microsoft.Maps.Location(lat, lng)
         const pin = new Microsoft.Maps.Pushpin(location, {
@@ -90,12 +100,12 @@ $(() => {
     }
 
     const park = {
-        name: ["USJ", "ハウステンボス", "ディズニーランド", "横浜シーパラダイス", "白い恋人パーク", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
-        place: ["大阪", "長崎", "東京", "神奈川", "北海道", "https://www.fujiq.jp/", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
-        phone: ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",],
-        web: ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",],
-        latitude: [34.665442, 56, 45, 52, 32],
-        longitude: [135.432338, 158, 52, 85, 152],
+        name: ["USJ", "ハウステンボス", "ディズニーランド", "横浜シーパラダイス", "白い恋人パーク", "富士急ハイランド", "東映太秦映画村", "aa", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        place: ["大阪", "長崎", "東京", "神奈川", "北海道", "山梨", "京都", "aa", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        phone: ["0570-200-606", "0570-064-110", "045-330-5211", "045-788-8888", "011-666-1481", "0555-23-2111", "075-864-7716", "aa", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",],
+        web: ["https://www.usj.co.jp/", "http://www.huistenbosch.co.jp/", "http://www.seaparadise.co.jp/", "http://www.shiroikoibitopark.jp/", "https://www.fujiq.jp/", "http://www.toei-eigamura.com/", "aa", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",],
+        latitude: [34.665442, 33.088755, 35.6359322, 35.3368011, 43.0882755, 35.4851444, 35.016189, 55],
+        longitude: [135.432338, 129.7863526, 139.8786311, 139.6456091, 141.2708274, 138.7773048, 135.7086368, 152],
     };
 
     function oll_open() {
@@ -110,4 +120,66 @@ $(() => {
 
         }
     }
+
+    $('#map_open').on('click', function () {
+
+        getPosition();
+
+    });
+
+    $('#comand_buttun').on('click', function () {
+        var input_message = document.getElementById("comand_text").value;
+        let parent = document.getElementById('tuika');
+        let chack = 0;
+        parent.innerHTML = ''; //子要素を全て削除
+        for (let i = 0; i < park.name.length; i++) {
+            if (input_message == park.name[i]) {
+                var newElement_1 = document.createElement("p"); // p要素作成
+                var newElement_2 = document.createElement("p"); // p要素作成
+                var newElement_3 = document.createElement("p"); // p要素作成
+                var newElement_4 = document.createElement("a"); // p要素作成
+                var newContent_1 = document.createTextNode(park.name[i]); // テキストノードを作成
+                var newContent_2 = document.createTextNode(park.place[i]); // テキストノードを作成
+                var newContent_3 = document.createTextNode(park.phone[i]); // テキストノードを作成
+                var newContent_4 = document.createTextNode(park.web[i]); // テキストノードを作成
+                newElement_1.appendChild(newContent_1); // p要素にテキストノードを追加
+                newElement_2.appendChild(newContent_2); // p要素にテキストノードを追加
+                newElement_3.appendChild(newContent_3); // p要素にテキストノードを追加
+                newElement_4.appendChild(newContent_4); // p要素にテキストノードを追加
+                newElement_4.setAttribute("id", "a"); // p要素にidを設定
+
+                var parentDiv = document.getElementById("tuika");
+
+                parentDiv.insertBefore(newElement_4, parentDiv.firstChild);
+                parentDiv.insertBefore(newElement_3, parentDiv.firstChild);
+                parentDiv.insertBefore(newElement_2, parentDiv.firstChild);
+                parentDiv.insertBefore(newElement_1, parentDiv.firstChild);
+
+                const target = document.getElementById("a")
+
+                const changeHref = () => {
+                    target.href = web[i]
+                    target.textContent = "Yahoo"
+                }
+
+                chack = 1;
+
+                let lat = park.latitude[i];
+                let lng = park.longitude[i];
+                let name = park.name[i];
+                let place = park.place[i];
+
+                mapsInit_2(lat, lng, map, name, place);
+            }
+        }
+        if (chack == 0) {
+            var newElement_1 = document.createElement("p"); // p要素作成
+            var newContent_1 = document.createTextNode("その遊園地は登録されていないもしくは存在しません。");
+            newElement_1.appendChild(newContent_1);
+            var parentDiv = document.getElementById("tuika");
+            parentDiv.insertBefore(newElement_1, parentDiv.firstChild);
+        } else {
+            chack = 0;
+        }
+    });
 })
